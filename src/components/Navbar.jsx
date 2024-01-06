f9c9d0d5942bf210f6522d45afa00b11d7ebfd05
AdminForIinRage <bhattaraianjesh123@gmail.com>
2024-01-07 01:12:34 +1030

    done :))

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,24 @@
 import { Link } from 'react-router-dom'
+import { useTheme } from '../hook/useTheme'
 
 // styles
 import './Navbar.css'
-import SearchBar from './SearchBar'
+import Searchbar from './Searchbar'
+import { useState } from 'react'
 
 export default function Navbar() {
+
+  const { color } = useTheme()
+
+
+
   return (
-    <div className="navbar">
-      <nav>
+    <div className="navbar" style={{ background: color }}>
+      <nav >
         <Link to="/" className="brand">
           <h1>Cooking Ninja</h1>
         </Link>
-        <SearchBar />
+        <Searchbar />
         <Link to="/create">Create Recipe</Link>
       </nav>
     </div>
--- src/pages/recipe/Recipe.jsx
+++ src/pages/recipe/Recipe.jsx
@@ -1,37 +1,30 @@
-import { useParams } from "react-router-dom";
-import { useFetch } from "../../hook/useFetch";
-import "./Recipe.css";
+import { useParams } from 'react-router-dom'
+import { useFetch } from '../../hook/useFetch'
+import { useTheme } from '../../hook/useTheme'
+
+// styles
+import './Recipe.css'
 
 export default function Recipe() {
-  const { id } = useParams();
-  const url = "http://localhost:3000/recipes/" + id;
-  const { data: recipes, isPending, error } = useFetch(url);
-
-
- 
-
-
+  const { id } = useParams()
+  const url = 'http://localhost:3000/recipes/' + id
+  const { error, isPending, data: recipe } = useFetch(url)
+  const { mode } = useTheme()
 
   return (
-    <div className="recipe">
-      {error && <div className="error"></div>}
-      {isPending && <div className="loading">loading...</div>}
-      {recipes && (
+    <div className={`recipe ${mode}`}>
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
+      {recipe && (
         <>
-          <h2 className="page-title">{recipes.title}</h2>
-          <p>Takes {recipes.cookingTime} to cook.</p>
-
+          <h2 className="page-title">{recipe.title}</h2>
+          <p>Takes {recipe.cookingTime} to cook.</p>
           <ul>
-            {recipes.ingredients.map(ingredient => (
-             <li key={ingredient}> {ingredient}</li>
-              
-            ))}
+            {recipe.ingredients.map(ing => <li key={ing}>ing</li>)}
           </ul>
-          <p className="method">
-            {recipes.method}
-          </p>
+          <p className="method">{recipe.method}</p>
         </>
       )}
     </div>
-  );
+  )
 }
